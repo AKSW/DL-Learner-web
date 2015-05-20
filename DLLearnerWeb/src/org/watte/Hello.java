57cf6627483dfffb312cffe645379a16f405f728
Patrick Watzlawik <patrick.watzlawik@web.de>
2015-05-20 16:37:30 +0200

    Added AKSW Repo and DLLearner Dependency in POM

--- DLLearnerWeb/src/org/watte/Hello.java
+++ DLLearnerWeb/src/org/watte/Hello.java
@@ -3,12 +3,17 @@ package org.watte;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import org.dllearner.cli.CLI;
+
 @Path("/hello")
 public class Hello {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String boo() {
+		
+		CLI cli = new CLI();
+		
 		return "aasdaüüüüüüüüasdsd";
 	}
 	
